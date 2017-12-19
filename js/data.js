ea3b2008ef82669f0994c0d67b8f78859cd74bab
sasha-belokur <alex.belokur17@gmail.com>
2017-12-19 16:57:05 +0200

    in data.js added functions dataLoadHandler and addIDProperty

--- js/data.js
+++ js/data.js
@@ -5,14 +5,19 @@
     advertisements: null
   };
 
-  window.backend.load(function (data) {
-    var advertisements = data.slice();
-
-    advertisements.forEach(function (obj, i) {
+  var addIDProperty = function (array) {
+    array.forEach(function (obj, i) {
       obj.id = i;
     });
+  };
 
+  var dataLoadHandler = function (data) {
+    var advertisements = data.slice();
+
+    addIDProperty(advertisements);
     window.data.advertisements = advertisements;
-  }, window.backend.errorHandler);
+  };
+
+  window.backend.load(dataLoadHandler, window.backend.errorHandler);
 
 })();
