42ded75bfbe51038a1fde65f42a9051373a9f910
sasha-belokur <alex.belokur17@gmail.com>
2017-12-19 16:16:09 +0200

    replaced inputs initialization from functions to module scope, in getPins replaced logic chain with filter chain

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,21 @@
 'use strict';
 (function () {
+  var typeFilter = document.querySelector('#housing-type');
+  var priceFilter = document.querySelector('#housing-price');
+  var roomsFilter = document.querySelector('#housing-rooms');
+  var guestsFilter = document.querySelector('#housing-guests');
+  var features = Array.from(document.querySelectorAll('#housing-features input'));
+  var MIN_MID_PRICE = 10000;
+  var MAX_MID_PRICE = 50000;
+
   var isFitType = function (ad) {
-    var typeFilterValue = document.querySelector('#housing-type').value;
-    return typeFilterValue === 'any' ? true : ad.offer.type === typeFilterValue;
+    return typeFilter.value === 'any' ? true : ad.offer.type === typeFilter.value;
   };
 
   var isFitPrice = function (ad) {
-    var MIN_MID_PRICE = 10000;
-    var MAX_MID_PRICE = 50000;
-    var priceFilterValue = document.querySelector('#housing-price').value;
     var result = false;
 
-    switch (priceFilterValue) {
+    switch (priceFilter.value) {
       case 'any':
         result = true;
         break;
@@ -29,33 +33,34 @@
     return result;
   };
 
-  function isFitRooms(ad) {
-    var roomsFilterValue = document.querySelector('#housing-rooms').value;
-    return roomsFilterValue === 'any' ? true : ad.offer.rooms === +roomsFilterValue;
-  }
+  var isFitRooms = function (ad) {
+    return roomsFilter.value === 'any' ? true : ad.offer.rooms === +roomsFilter.value;
+  };
 
-  function isFitGuests(ad) {
-    var guestsFilterValue = document.querySelector('#housing-guests').value;
-    return guestsFilterValue === 'any' ? true : ad.offer.guests === +guestsFilterValue;
-  }
+  var isFitGuests = function (ad) {
+    return guestsFilter.value === 'any' ? true : ad.offer.guests === +guestsFilter.value;
+  };
 
-  function isFitFeatures(ad) {
-    var checkedFeatures = Array.from(document.querySelectorAll('#housing-features input'));
-    checkedFeatures = checkedFeatures.filter(function (input) {
-      return input.checked;
-    })
+  var isFitFeatures = function (ad) {
+    var checkedFeatures = features
+        .filter(function (input) {
+          return input.checked;
+        })
         .map(function (input) {
           return input.value;
         });
 
     return window.util.isSubArray(checkedFeatures, ad.offer.features);
-  }
+  };
 
-  function getPins() {
-    return window.data.advertisements.slice().filter(function (ad) {
-      return isFitType(ad) && isFitPrice(ad) && isFitRooms(ad) && isFitGuests(ad) && isFitFeatures(ad);
-    });
-  }
+  var getPins = function () {
+    return window.data.advertisements
+        .filter(isFitType)
+        .filter(isFitPrice)
+        .filter(isFitRooms)
+        .filter(isFitGuests)
+        .filter(isFitFeatures);
+  };
 
   window.filter = {
     getPins: getPins
